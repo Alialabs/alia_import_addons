63fa34693627aa6a62b6cb661ee4800277d04b17
castor <castorsanchez@alialabs.com>
2018-05-15 03:24:45 +0200

    INIT: First commit

---
+++ alia_base_imports/alia_base_excel_file_handler.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+#
+#    Created on 9/05/18
+#
+#    @authors:loxo,cástor
+#
+#
+# 2018 ALIA Technologies
+#       http://www.alialabs.com
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+import openpyxl as openpyxl
+import base64,io
+from openpyxl.utils import get_column_letter
+
+
+class AliaBaseExcelFileHandler:
+    """
+    Excel Read, Load and Save tools
+    """
+    _sheets_values = None  # type: dict
+
+    def __init__(self, filepath=False):
+        """
+        :param filepath: string - book file path
+        """
+        self._filepath = filepath
+        self._work_book = False
+        self._sheet_names = False
+        self._sheets_values = False
+
+    def load_workbook(self, decode64, book_file_name=False):
+        """
+        Load workbook
+        :param book_file_name: string - book file path
+        :return:
+        """
+        filepath = book_file_name or self._filepath
+        if decode64:
+            decoded_data = base64.b64decode(filepath)
+            xls_file = io.BytesIO(decoded_data)
+            self._work_book = openpyxl.load_workbook(xls_file)
+            self._sheet_names = self._work_book.sheetnames
+        else:
+            self._work_book = openpyxl.load_workbook(filepath)
+            self._sheet_names = self._work_book.sheetnames
+
+
+    def _pos_process_row_dict(self, row_dict):
+        """
+        Hook to pos process row dict
+        :param row_dict: dictionary {'row_ref': {row}}
+        :return: dictionary {'row_ref': {row}}
+        """
+        return row_dict
+
+    def get_row_dict_by_column_letter(self, row, sheet, max_column):
+        """
+
+        :param row:
+        :return:
+        """
+        row_dict = {}
+        for c in range(1, max_column):
+            column_letter = get_column_letter(c)
+            row_dict[column_letter] = sheet[column_letter + str(row)].value
+        row_dict = self._pos_process_row_dict(row_dict)
+        return row_dict
+
+    def get_row_dict_by_title(self, row, sheet, max_column, row_title=1):
+        """
+
+        :param row:
+        :return:
+        """
+        row_dict = {}
+        for c in range(1, max_column):
+            column_letter = get_column_letter(c)
+            row_dict[sheet[column_letter + str(row_title)].value] = sheet[column_letter + str(row)].value
+        row_dict = self._pos_process_row_dict(row_dict)
+        return row_dict
+
+    def get_sheets(self):
+        """
+        Return a sheets list
+        """
+        return self._work_book._sheets
+
+
+    def get_sheet_dict(self, sheet_name, row_ref=False):
+        """
+        Return a dictionary with sheet rows as dictionary with row row_ref column as row key
+        :param sheet_name: string - sheet name
+        :param row_ref: string - reference as key to identify rows in sheet dictionary.
+        :return: dictionary {'row_ref': {row}}
+        """
+        sheet = self._work_book[sheet_name]
+        max_column = sheet.max_column
+        max_row = sheet.max_row + 1
+
+        if not row_ref:
+            row_ref = sheet['A' + str(1)].value
+
+        toret_dict = {}
+        for row in range(2, max_row):
+            row_dict = self.get_row_dict_by_title(row, sheet, max_column)
+            toret_dict[row_dict[row_ref]] = row_dict
+
+        return toret_dict
+
+    def get_sheet_list(self, sheet_name, row_ref=False):
+        """
+        Return a dictionary with sheet rows as dictionary with row row_ref column as row key
+        :param sheet_name: string - sheet name
+        :param row_ref: string - reference as key to identify rows in sheet dictionary.
+        :return: dictionary {'row_ref': {row}}
+        """
+        sheet = self._work_book[sheet_name]
+        max_column = sheet.max_column
+        max_row = sheet.max_row + 1
+
+        toret = []
+        for row in range(2, max_row):
+            row_dict = self.get_row_dict_by_column_letter(row, sheet, max_column)
+            toret.append(row_dict)
+
+        return toret
+
+    def _get_dynamic_sheet_load_method_and_type(self, container_type):
+        """
+        Return the method to load the sheets values.
+        :param container_type: string - container type
+        :return: tuple - (type, method call without parameters)
+        """
+        if container_type and container_type == 'dict':
+            return {}, self.get_sheet_dict
+        else:
+            return [], self.get_sheet_list
+
+    def load_sheets(self, container_type=False):
+        """
+        Return a dictionary of sheets
+        :return: dictionary {'sheet_name': {sheet}}
+        """
+        get_sheet_dinamyc_method_and_type = self._get_dynamic_sheet_load_method_and_type(container_type)
+        sheets_vals = {}
+        for sheet_name in self._sheet_names:
+            sheets_vals[sheet_name] = get_sheet_dinamyc_method_and_type[1](sheet_name)
+        self._sheets_values = sheets_vals
+
+        return sheets_vals
+
+    def get_rows_in_list(self, sheet_data_dict):
+        """
+        Process a dict an return a list of values
+        :param sheet_data_dict:
+        :return:
+        """
+        list_toret = []
+        for key in sheet_data_dict.keys():
+            for row in sheet_data_dict[key]:
+                list_toret.append(row)
+        return list_toret
+
+    @staticmethod
+    def save_first_line(sheet, first_line_vals):
+        """
+        Save the first line in a sheet. Column Headers
+        :param sheet: openpyxl sheet object
+        :param first_line_vals: dictionary - {'A': value, ...}
+        :return: sheet
+        """
+        index = 0
+        for row in sheet.iter_cols(min_row=1, min_col=1, max_col=len(first_line_vals)):
+            for cell in row:
+                cell.value = first_line_vals.keys()[index]
+                index += 1
+        return sheet
+
+    @staticmethod
+    def save_rows(sheet, sheet_rows_data_list, start_range):
+        """
+        Save rows values on sheet
+        :param sheet: openpyxl sheet object
+        :param sheet_rows_data_list: list
+        :param start_range: interger - index to start save rows
+        :return: sheet
+        """
+        for row in range(start_range, len(sheet_rows_data_list) + start_range):
+            for col in range(1, len(sheet_rows_data_list[row-start_range].keys())):
+                sheet.cell(column=col, row=row, value=sheet_rows_data_list[row-start_range].values()[col-1])
+        return sheet
+
+    def save_data_in_workbook(self, data_sheets, filename, first_line=[]):
+        """
+        Save sheets values on workbook
+        :param data_sheets: dictionary - {'sheet_name': [{row}, ..]}
+        :param filename: string - name of the output file
+        :param first_line: [] - sheet names when the first row is column header
+        """
+        new_wb = openpyxl.Workbook()
+        sheet_index = 0
+        for sheet_name in data_sheets.keys():
+            new_wb.create_sheet(index=sheet_index, title=sheet_name)
+            new_sheet = new_wb[sheet_name]
+
+            start_range = 1
+            if sheet_name in first_line:
+                self.save_first_line(new_sheet, data_sheets[sheet_name][0])
+                start_range = 2
+
+            self.save_rows(new_sheet, data_sheets[sheet_name], start_range)
+
+        new_wb.save(filename)
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ alia_partners_import/wizard/alia_partners_import_wizard.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+####################################
+#
+#    Created on 6 de abril de 2018
+#
+#    @author:castor
+#
+##############################################################################
+#
+# 2016 ALIA Technologies
+#       http://www.alialabs.com
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+
+import logging
+import sys
+import openpyxl,base64,io
+from osv import fields, osv
+from tools.translate import _
+from pip.utils import file_contents
+from datetime import datetime
+
+
+_logger = logging.getLogger(__name__)
+_schema = logging.getLogger(__name__ + '.schema')
+
+
+
+
+class alia_partner_to_import(osv.osv_memory):
+    _name = 'alia.partner.to.import'
+    
+    _columns = {
+                'name':fields.char('Name',size=128),
+                'street':fields.char('Street',size=128),
+                'vat':fields.char('VAT',size=128),
+                'zip':fields.char('ZIP',size=128),
+                'ref':fields.char('Reference',size=128),
+                'city':fields.char('City',size=128),
+                'customer':fields.boolean('customer'),
+                'supplier':fields.boolean('supplier'),
+                'company_id':fields.many2one('res.partner'),
+                'wizard_id':fields.many2one('alia.partners.import.wizard'),
+                }
+ 
+
+
+class alia_partners_import_wizard(osv.osv_memory):
+    _name = 'alia.partners.import.wizard'
+
+    _columns = {
+               'name':fields.char('Name',size=128),
+               'file_to_import':fields.binary('File to import',required=True),
+               'omit_last_rows':fields.integer('Last rows to be omitted'),
+               'omit_init_rows':fields.integer('Init rows to be omitted'),
+               'one_step_wizard':fields.boolean('Wizard without checking, directly creation'),
+               'company_id': fields.many2one('res.company', 'Company', required=True),
+               'vat_prefix':fields.char('VAT prefix',size=128),
+               'omit_incorrect_vat':fields.boolean('Omit incorrect VATs'),
+               'type_address': fields.selection([('default','default'),('invoice','invoice'),('delivery','delivery'),('other','other')]),
+               'partners_list': fields.one2many('alia.partner.to.import','wizard_id'),
+               'state':fields.selection([('preprocess','preprocess'),('aligment','aligment')])
+               }
+    
+    _defaults = {
+                 'name':"Wizard Import",
+                 'state':'preprocess',
+                 }
+    
+    
+    
+    
+    def get_vat_number(self,cr,uid,wzd,vat,context=None):
+        """
+        Description...
+        """
+        if not vat:
+            return False
+        toret = str(wzd.vat_prefix)+str(vat)
+        vat_country, vat_number = toret[:2].lower(), toret[2:].replace(' ', '')
+        if wzd.omit_incorrect_vat:
+            return toret if self.pool.get('res.partner').simple_vat_check(cr,uid,vat_country,vat_number,context=context) else False
+        else:
+            return toret
+  
+    
+    def process_file(self,cr,uid,ids,context=None):
+        """
+        Description...
+        """
+        ir_model_data = self.pool['ir.model.data']
+        model_data_id = ir_model_data._get_id(cr, uid, 'alia_partners_import', 'alia_partners_import_process_wizard_view')
+        res_id = ir_model_data.browse(cr, uid, model_data_id, context=context).res_id
+        try:
+            wizard_main_obj = self.browse(cr, uid, ids[0])
+            decoded_data = base64.b64decode(wizard_main_obj.file_to_import)
+            xls_file = io.BytesIO(decoded_data)
+            wb = openpyxl.load_workbook(xls_file)
+            for sheet in wb._sheets:
+                max_column = sheet.max_column
+                max_rows = sheet.max_row - wizard_main_obj.omit_last_rows
+                init_row_range = 1 + wizard_main_obj.omit_init_rows
+                vals = []
+                for row in sheet.iter_rows(min_row=init_row_range,max_col=max_column,max_row=max_rows):
+                    row_dict = {}
+                    row_dict['ref'] = row[0].value
+                    row_dict['name'] = row[1].value
+                    row_dict['vat'] = self.get_vat_number(cr,uid,wizard_main_obj,row[2].value,context)
+                    row_dict['street'] = row[3].value
+                    row_dict['city'] = row[4].value
+                    row_dict['zip'] = row[5].value
+                    row_dict['customer'] = True if int(row[6].value) > 0 else False
+                    row_dict['supplier'] = True if int(row[7].value) > 0 else False
+                    row_dict['company_id'] = wizard_main_obj.company_id.id
+                    vals.append((0,0,row_dict))                                                  
+            self.write(cr,uid,ids,{'partners_list':vals,'state':'aligment'},context=context)
+            
+            if wizard_main_obj.one_step_wizard:
+                self.create_partners(cr, uid, [wizard_main_obj.id], context)
+            else:
+                return {
+                    'type':'ir.actions.act_window',
+                    'res_model':'alia.partners.import.wizard',
+                    'view_id':res_id,
+                    'view_mode':'form',
+                    'view_type':'form',
+                    'res_id':ids[0],
+                    'views':[(res_id,'form')],
+                    'target':'new',
+                    } 
+        except Exception as e:
+            _logger.error(e)
+            raise
+        
+ 
+        
+    def create_partners(self,cr,uid,ids,context=None):
+        """
+        Description 
+        """
+        wzd = self.pool.get('alia.partners.import.wizard').browse(cr,uid,ids[0],context=context)
+        for partner in wzd.partners_list:
+            vals_partner = {}
+            vals_address = {}
+            vals_address['street'] = partner.street
+            vals_address['zip'] = partner.zip
+            vals_address['city'] = partner.city
+            vals_address['type'] = 'default'  
+            vals_partner['name'] = partner.name
+            vals_partner['vat'] = partner.vat
+            vals_partner['ref'] = partner.ref
+            vals_partner['customer'] = partner.customer
+            vals_partner['supplier'] = partner.supplier
+            vals_partner['company_id'] = partner.company_id.id
+            vals_partner['address'] = [(0,0,vals_address)]
+            partner_id = self.pool.get('res.partner').create(cr,uid,vals_partner,context=context)
+            _logger.info("Partner Created: %d",partner_id)         
+        return {
+                'type':'ir.actions.act_window_close',
+        }
+        
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
